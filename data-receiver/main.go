d6ec297a456b8d1a547b18195298b93db8a09661
kkboranbay <kkboranbay@gmail.com>
2023-10-07 15:58:17 +0600

    Setting up the Kafka producer

--- data-receiver/main.go
+++ data-receiver/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gorilla/websocket"
 	"github.com/kkboranbay/toll-calculator/types"
 )
 
+var kafkaTopic = "obudata"
+
+// go get github.com/confluentinc/confluent-kafka-go/kafka
 func main() {
-	recv := NewDataReceiver()
+	recv, err := NewDataReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/ws", recv.handleWS)
 	http.ListenAndServe(":30000", nil)
 }
@@ -18,12 +26,48 @@ func main() {
 type DataReceiver struct {
 	msgch chan types.OBUData
 	conn  *websocket.Conn
+	prod  *kafka.Producer
 }
 
-func NewDataReceiver() *DataReceiver {
-	return &DataReceiver{
-		msgch: make(chan types.OBUData, 128),
+func NewDataReceiver() (*DataReceiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
 	}
+
+	// Start another goroutine to check if we have delivered the data.
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+	return &DataReceiver{
+		// channel always block when its full
+		msgch: make(chan types.OBUData, 128),
+		prod:  p,
+	}, nil
+}
+
+func (dr *DataReceiver) produceData(data types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Produce messages to topic (asynchronously)
+	dr.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &kafkaTopic, Partition: kafka.PartitionAny},
+		Value:          b,
+	}, nil)
+
+	return nil
 }
 
 func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
@@ -53,8 +97,13 @@ func (dr *DataReceiver) wsReceiveLoop() {
 			continue
 		}
 
-		fmt.Printf("received OBU data from [%d] :: <lat %.2f, long: %.2f> \n", data.OBUID, data.Lat, data.Long)
+		if err := dr.produceData(data); err != nil {
+			log.Println("kafka product error:", err)
+		}
 
-		dr.msgch <- data
+		// so when we run first, make receiver, then open another terminal, there run make obu
+		// in recerver terminal we get some messages but after when channel is full, then its not get a new data
+		// because channel is full, nobody is consuming!!!
+		// dr.msgch <- data
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kkboranbay/toll-calculator
 
 go 1.20
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
